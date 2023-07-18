9d0a9331c9223f1102b5f9cc64d772906cbeccf8
Anrian Sihotang <anrisihotang91@gmail.com>
2023-07-18 17:10:54 +0700

    day update ziggity content and location

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,13 +2,16 @@ import React from "react";
 import About from "../components/About";
 import Product from "../components/Product";
 import Best from "../components/BestProduct";
-
+import Ziggity from "../components/Ziggity";
+import Location from "../components/location";
 const Main = () => {
 	return (
 		<main>
 			<About/>
 			<Product/>
 			<Best/>
+			<Ziggity/>
+			<Location/>
 		</main>
 	)
 }
